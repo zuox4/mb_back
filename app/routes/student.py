029c33dd54546cf0f2ed51ca76b1a35a62d41afa
zuox4 <140226434+zuox4@users.noreply.github.com>
2026-01-28 14:30:54 +0300

    add_important_events for student

--- app/routes/student.py
+++ app/routes/student.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from app.database.database import get_db
 from fastapi import APIRouter, Depends, HTTPException
 
-from app.database.models import User, ProjectOffice, Group
+from app.database.models import User, ProjectOffice, Group, p_office_event_association
 
 from app.auth.dependencies import get_current_active_user, get_current_user
 from app.routes.mark_book import RecordBookResponse, get_student_record_book_marks_optimized
@@ -21,6 +21,7 @@ class Event(BaseModel):
     id: int
     title: str
     is_active: bool
+    is_important: bool
 
 class ProjectOfficeResponse(BaseModel):
     title: str
@@ -102,11 +103,18 @@ def get_project_office_info(current_user: User = Depends(get_current_active_user
     if not ('student'in [role.name for role in current_user.roles]):
         raise HTTPException(status_code=401, detail="Нет доступа в соответствии с ролью")
     projects_office = db.query(ProjectOffice).join(ProjectOffice.accessible_classes).filter(Group.name==current_user.group_name).first()
-
+    x = db.query(p_office_event_association).filter(p_office_event_association.c.p_office_id == projects_office.id).all()
+    impotant_ids = [i.event_id for i in x if i.is_important == True]
     if projects_office is None:
         raise HTTPException(status_code=404, detail=str('Проект для пользователя не найден'))
-
-    return projects_office
+    data = {
+        'title': projects_office.title,
+        'description': projects_office.description,
+        'logo_url': projects_office.logo_url,
+        'accessible_events': [{'id': i.id, 'title': i.title, 'is_active': True,
+    'is_important': i.id in impotant_ids} for i in projects_office.accessible_events]
+    }
+    return data
 
 
 @router.get("/record-book/marks", response_model=RecordBookResponse)
